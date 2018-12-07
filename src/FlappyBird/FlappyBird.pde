63c8135e69151bb7b47daac2bfb4805b1d633bd0
Calvin Page <league@WTS-IM-17.attlocal.net>
2018-12-06 16:59:35 -0800

    started flappy bird

---
+++ src/FlappyBird/FlappyBird.pde
@@ -0,0 +1,40 @@
+int birdY;
+int yspd = 0;
+int pipeX = 800;
+int pipeT = (int) random(100,400);
+int gamestate;
+void draw(){
+  background(180,220,255);
+  fill(255,50,50);
+  stroke(0,0,0);
+  if(gamestate==0){
+  ellipse(100,birdY,50,50);
+  fill(50,255,50);
+    if(pipeX<-100){
+    pipeX = 800;
+    pipeT = (int) random(100,500);
+  }
+  
+  rect(pipeX,pipeT+200,100,600-pipeT);
+  rect(pipeX,-1,100,pipeT);
+  pipeX = pipeX -5;
+
+  
+  yspd = yspd+1;
+  birdY = birdY + yspd;
+  if(keyPressed){
+   yspd = -10; 
+  }
+  }
+  if(birdY-25 < pipeT && pipeX > 75 && pipeX < 125){
+    gamestate = 1;
+  }
+  if(birdY > pipeT + 175 && pipeX > 75 && pipeX < 125){
+   gamestate = 1; 
+  }
+  //birdX is always 100
+  //bird has 25 radius
+}
+void setup(){
+size(800,800);
+}
--- src/extra/GridWorld.java
+++ src/extra/GridWorld.java
@@ -1,6 +1,10 @@
 package extra;
 
+import java.awt.Color;
+import java.util.Random;
+
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
@@ -10,7 +14,46 @@ public static void main(String[] args) {
 	World wrld = new World();
 	wrld.show();
 	Bug bug = new Bug();
-	Location loc = new Location(1,3);
+	Location loc = new Location(3,4);
+	wrld.add(loc, bug);
+	Random rand = new Random();
+	int rloca = rand.nextInt(9);
+	int rlocb = rand.nextInt(9);
+	Location loc2 = new Location(rloca,rlocb);
+	Bug bug2 = new Bug(Color.blue);
+	wrld.add(loc2, bug2);
+	bug2.turn();
+	bug2.turn();
+	
+	Flower fl = new Flower(Color.pink);
+	Flower fr = new Flower(Color.CYAN);
+	Location fll = new Location(rloca, rlocb-1);
+	Location frl = new Location(rloca, rlocb+1);
+	wrld.add(fll, fl);
+	wrld.add(frl, fr);
+	Flower flow = new Flower(Color.RED);
+	Flower flowwhite = new Flower(Color.WHITE);
+	Flower flowblue = new Flower(Color.BLUE);
+	for (int x = 0; x < 10; x++) {
+	for (int i = 0; i < 10; i++) {
+		Location locc = new Location(i,x);
+		if(x<5 && i<5) {
+		if(i%2==0 || x%2==0) {
+			wrld.add(locc, flowblue);
+		}else {
+			wrld.add(locc, flowwhite);
+		}
+		
+		
+		}else if(i%2==0) {
+		wrld.add(locc, flow);
+		}else {
+		wrld.add(locc, flowwhite);
+		}
+		System.out.println("loop?");
+	}
+	}
+	
 	
 }
 }
